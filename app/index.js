06564a8f5ce25e111dea7f7ba431fbebe5744e36
Sudarshan <mrsudarshank8124@gmail.com>
2025-12-10 23:01:31 +0530

    Completed: Supabase Setup + Premium Theme

--- app/index.js
+++ app/index.js
@@ -13,17 +13,22 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-3xl font-bold mb-4">
+    <View className="flex-1 items-center justify-center bg-cream">
+      <Text className="text-obsidian text-4xl font-bold mb-2 tracking-widest">
         TiffinTales 🍱
       </Text>
       <Text className="text-gray-400 text-lg mb-8">
         Home Cooked Food, Delivered.
       </Text>
       
-      <Link href="/login" className="bg-orange-500 px-6 py-3 rounded-full">
-        <Text className="text-white font-bold">Get Started</Text>
+      <Link href="/login" className="bg-obsidian px-10 py-4 rounded-full shadow-lg">
+        <Text className="text-cream font-bold text-xl">Get Started</Text>
       </Link>
+
+
+      <View className="absolute bottom-10">
+        <Text className="text-gray-400 text-xs">POWERED BY SUDARSHAN</Text>
+      </View>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./app/**/*.{js,jsx,ts,tsx}"  // <--- THIS TELLS IT TO LOOK IN THE APP FOLDER
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        obsidian :'#1A0B2E',
+        cream : '#FDFBF7',
+        brige : '#F5E6D3',
+        gold : '#FFD700',
+
+
+        primary : '#1A0B2E',
+        background : '#FDFBF7',
+        card :'#FFFFFF',
+        text : "#1A0B2E",
+    },
   },
+},
   plugins: [],
 }
